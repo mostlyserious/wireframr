df917d998696e0e5078db05816fbdd025f2882fb
Zack Bonebrake <gigonaut@Mad-Hatter-Mobile.local>
2012-07-09 22:33:34 -0500

    forgot to clear the silly comment.

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
 	$('a').click(function() {
 		var to = $(this).attr('href');
 		$('html,body').animate({scrollTop: $(to).offset().top - 50},'slow');
-		// add pushstate here
 		history.pushState({foo: 'bar'}, window.document.title, to);
 		return false;
 	});
